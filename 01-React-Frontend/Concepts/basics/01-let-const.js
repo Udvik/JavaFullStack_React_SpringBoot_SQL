9a769f0b541cf595b4341a79474704e93e3a5d41
udvik <udvik4321@gmail.com>
2026-03-01 23:20:26 +0530

    contains basics required for react

---
+++ 01-React-Frontend/Concepts/basics/01-let-const.js
@@ -0,0 +1,10 @@
+function scope(){
+  if(true){
+    let a = 10;
+    const b = "Udvik";
+    console.log(a,b);
+  }
+  //console.log(a);
+}
+
+scope();
---
+++ 01-React-Frontend/Concepts/basics/02-arrow-functions.js
@@ -0,0 +1,8 @@
+function add(a,b){
+  return a+b;
+}
+
+const multiply = (a,b) => a*b;
+
+console.log(add(2,3));
+console.log(multiply(2,3));
