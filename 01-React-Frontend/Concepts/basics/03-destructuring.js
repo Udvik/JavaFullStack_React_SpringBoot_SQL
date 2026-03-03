9c492dd2a0c17590e5fb8efa33e5e933fcedbb10
udvik <udvik4321@gmail.com>
2026-03-03 22:45:23 +0530

    Revised Destructuring

---
+++ 01-React-Frontend/Concepts/basics/03-destructuring.js
@@ -0,0 +1,9 @@
+
+const user = { name: "Udvik", role: "Developer" }
+const { name, role } = user
+console.log(name, role)
+
+  
+const numbers = [10, 20, 30]
+const [first, second] = numbers
+console.log(first, second)
